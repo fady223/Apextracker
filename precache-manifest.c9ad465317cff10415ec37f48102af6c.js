9e5c7b5d3b8b8967619c9d84fdcb503ea42c2054
fady223 <fadyshaker2251151@gmail.com>
2020-07-05 14:23:06 +0200

    Updates

---
+++ precache-manifest.c9ad465317cff10415ec37f48102af6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0effd0423e8a1ac4a558a7d286fe2b",
+    "url": "/Apextracker/index.html"
+  },
+  {
+    "revision": "592a76e0f569f9daf18f",
+    "url": "/Apextracker/static/css/main.e41cfae0.chunk.css"
+  },
+  {
+    "revision": "b44f1765f261054b8d5a",
+    "url": "/Apextracker/static/js/2.3a1718f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Apextracker/static/js/2.3a1718f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592a76e0f569f9daf18f",
+    "url": "/Apextracker/static/js/main.26c77202.chunk.js"
+  },
+  {
+    "revision": "a66b6374c0873b95eda1",
+    "url": "/Apextracker/static/js/runtime-main.33890703.js"
+  },
+  {
+    "revision": "1f310d753daa27d8715a2d54da4a527f",
+    "url": "/Apextracker/static/media/apex-legends-icon.1f310d75.png"
+  }
+]);
